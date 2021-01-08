b2a6dc438930b863d98b32c34e1b10985268b398
TaysaMendes <taysa_smp@hotmail.com>
2021-01-08 20:45:00 -0300

    Add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,31 @@
+import './style.css';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+
+            App desenvolvido durante a semana DevSuperior
+
+            <div className="footer-icons">
+                <a href="https://www.youtube.com" target="_new">
+                    <YoutubeIcon />
+                </a>
+
+                <a href="https://www.linkedin.com" target="_new">
+                    <LinkedinIcon />
+                </a>
+
+                <a href="https://www.instagram.com" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
